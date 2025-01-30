ac0203830b3ec45b324f0707bd02bbde7e0df29f
Sai Yaswanth Reddy Legala <yaswanth0412@gmail.com>
2025-01-29 20:40:44 -0800

    changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,7 +20,7 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Sai Yaswanth Reddy",
+  username: "Abhisek Namala",
   title: "Hi all, I'm Yaswanth",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
@@ -176,23 +176,26 @@ const workExperiences = {
       company: "Grabon",
       companylogo: require("./assets/images/grabonLogo.jpeg"),
       date: "June 2023 – Jan 2024",
-      desc: ".",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       descBullets: [
-        "Wrote the entire Backend as a team of 2, using Dot.net,node js within 2 months, Redis  for message queues which handles 1Lakh members concurrently ",
-        " Wrote Frontend using angular Html, css,Javascript(Angular)"
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
       ]
     },
     {
       role: "Front-End Developer",
-      company: "Coundria",
-      companylogo: require("https://images.squarespace-cdn.com/content/v1/6401d9b26d970f640bf4239b/1677843223362-N2KU82RCVP9ATWFFT0WK/Conduira_Logo_nomargin.png?format=1500w"),
+      company: "Quora",
+      companylogo: require("./assets/images/quoraLogo.png"),
       date: "May 2017 – May 2018",
-      desc: [
-        "Dynamic professional adept at Angular 8, Material UI CSS, and Mobi quick Bootstrap.",
-        " Orchestrated a successful AWS to Azure database migration"
-      ]
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
-  
+    {
+      role: "Software Engineer Intern",
+      company: "Airbnb",
+      companylogo: require("./assets/images/airbnbLogo.png"),
+      date: "Jan 2015 – Sep 2015",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
   ]
 };
 
@@ -324,34 +327,44 @@ const blogSection = {
 
 // Talks Sections
 
-// const talkSection = {
-//   title: "TALKS",
-//   subtitle: emoji(
-//     "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
-//   ),
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
 
-//   talks: [
-//     {
-//       title: "Build Actions For Google Assistant",
-//       subtitle: "Codelab at GDG DevFest Karachi 2019",
-//       slides_url: "https://bit.ly/saadpasta-slides",
-//       event_url: "https://www.facebook.com/events/2339906106275053/"
-//     }
-//   ],
-//   display: true // Set false to hide this section, defaults to true
-// };
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
 
 // Podcast Section
 
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
 
-// // Resume Section
-// const resumeSection = {
-//   title: "Resume",
-//   subtitle: "Feel free to download my resume",
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
 
-//   // Please Provide with Your Podcast embeded Link
-//   display: true // Set false to hide this section, defaults to true
-// };
+// Resume Section
+const resumeSection = {
+  title: "Resume",
+  subtitle: "Feel free to download my resume",
+
+  // Please Provide with Your Podcast embeded Link
+  display: true // Set false to hide this section, defaults to true
+};
 
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
@@ -383,9 +396,10 @@ export {
   bigProjects,
   achievementSection,
   blogSection,
- 
+  talkSection,
+  podcastSection,
   contactInfo,
   twitterDetails,
   isHireable,
-    
+  resumeSection
 };
